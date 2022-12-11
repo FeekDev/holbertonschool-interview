b8fbcbc544afc7dac4dfcd9b6bb5d301cd2487fd
felipeholbi <4758@holbertonstudents.com>
2022-12-11 15:04:30 -0500

    fix idented

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,49 +1,53 @@
 #!/usr/bin/python3
 
-def canUnlockAll(boxes):   
-    if not boxes or ( type(boxes) != list):
+def canUnlockAll(boxes):
+    if not boxes or (type(boxes) != list):
         return False
-    keyList=[]
+    keyList = []
     hasMoreKeys = False
     countBoxes = len(boxes)
     boxesUnlock = setBoxesUnlock(countBoxes)
     for i in range(countBoxes):
-        if( type(boxes[i]) == list ):
+        if (type(boxes[i]) == list):
             if i == 0:
                 keyList.append(0)
-                setKeys(boxes[i], keyList,boxesUnlock)
+                setKeys(boxes[i], keyList, boxesUnlock)
             if boxesUnlock[i] == 0 and keyList.count(i) > 0:
-                keyList = unlockBox(keyList,boxesUnlock,boxes)  
+                keyList = unlockBox(keyList, boxesUnlock, boxes)
         else:
             return False
     if boxesUnlock.count(0) == 0:
         return True
     return False
 
-def unlockBox(keys,boxesUnlock,boxes):
+
+def unlockBox(keys, boxesUnlock, boxes):
     keys_copy = []
-    if len(keys) >0:
+    if len(keys) > 0:
         if keys[0] < len(boxesUnlock):
             boxesUnlock[keys[0]] = 1
-            if not boxes[keys[0]] or ( type(boxes[keys[0]]) == list):
-                setKeys(boxes[keys[0]],keys,boxesUnlock)
+            if not boxes[keys[0]] or (type(boxes[keys[0]]) == list):
+                setKeys(boxes[keys[0]], keys, boxesUnlock)
             keys.pop(0)
-            unlockBox(keys,boxesUnlock,boxes)
+            unlockBox(keys, boxesUnlock, boxes)
     return keys
 
-def setKeys(box, keyList,boxesUnlock):
+
+def setKeys(box, keyList, boxesUnlock):
     if box:
         for key in box:
             if isInteger(key):
                 if key < len(boxesUnlock) and keyList.count(key) == 0 and boxesUnlock[key] == 0:
-                    keyList.append(key) 
+                    keyList.append(key)
+
 
 def setBoxesUnlock(count):
     boxes = []
     for i in range(count):
         boxes.append(0)
-    return boxes 
-        
+    return boxes
+
+
 def isInteger(str):
     is_int = True
     try:
@@ -51,4 +55,4 @@ def isInteger(str):
     except ValueError:
         is_int = False
 
-    return is_int        
+    return is_int
